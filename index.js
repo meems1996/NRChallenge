c2ce96598450d621c1779dae6376f829cca173d5
Maria Shimkovska <mshimkov@stetson.edu>
2021-07-15 23:07:09 -0700

    spelling error oops

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const startApplication = () => {
     // Remove the first couple of arguments
     const filePaths = process.argv.slice(2);
 
-    // get the top 100 three word sequences and their frequences 
+    // get the top 100 three word sequences and their frequencies. 
     const top100WordSequences = getTop100WordSequences(filePaths);
 
     // Format the array of the top 100 word sequences and their frequencies to the following format: 
